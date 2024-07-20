8f07cdabdaa7c9f94570f72715cc21a2848f6dc3
Sriram N V <sriram.vilvanathan@gmail.com>
2024-07-20 19:13:03 +0530

    defined basic scenes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 import {k} from "./kaboomLoader.js";
+import { room1 } from "./scenes/room1.js";
+import { room2 } from "./scenes/room2.js";
+
+
+k.scene("room1", (previousData)=>{
+    room1();
+});
+
+k.scene("room2", (previousData)=>{
+    room2();
+});
+
+k.scene("intro", ()=>{
+
+});
+
+k.go("intro");
 

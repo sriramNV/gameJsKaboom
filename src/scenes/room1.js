64746388093d9480c2c399ec5474f19904aba109
Sriram <sriramnvilvanathan@gmail.com>
2024-07-21 23:40:25 +0530

    camera bug fixes

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,57 +1,54 @@
 import { makePlayer } from "../entities/player.js";
-import { setBackgroundColor, setMapColliders, setCameraZones, setMapColliders, } from "./roomUtils.js";
+import {
+  setBackgroundColor,
+  setMapColliders,
+  setCameraZones,
+  setCameraControls,
+} from "./roomUtils.js";
 
-export function room1(k, roomData){
-    setBackgroundColor(k,"#a2aed5");
+export function room1(k, roomData) {
+  setBackgroundColor(k, "#a2aed5");
 
-    k.camScale(4);
-    k.camPos(170,100);
-    k.setGravity(1000);
-    
+  k.camScale(4);
+  k.camPos(170, 100);
+  k.setGravity(1000);
 
-    const roomLayers = roomData.layers;
+  const roomLayers = roomData.layers;
 
-    const map = k.add([
-        k.pos(0,0),
-        k.sprite("room1")
-    ]);
+  const map = k.add([k.pos(0, 0), k.sprite("room1")]);
 
-    const colliders = [];
-    const positions = [];
-    const cameras = [];
+  const colliders = [];
+  const positions = [];
+  const cameras = [];
 
-
-    for (const layer of roomLayers ){
-        if(layer.name === "cameras"){
-            cameras.push(...layer.objects);
-        }
-
-        if(layer.name === "positions"){
-            positions.push(...layer.objects);
-            continue;
-        }
-        if (layer.name === "colliders"){
-            colliders.push(...layer.objects)
-            continue;
-        }
-    }
-    
-    setMapColliders(k, map, colliders);
-    setCameraZones(k,map,cameras);
-    
-
-    const player = k.add(makePlayer(k));
-
-    setCameraControls(k,player, map, roomData);
-
-    for (const position of positions){
-        if(position.name === "player"){
-            player.setPosition(position.x, position.y);
-            player.setControls();
-            player.setEvents();
-            player.enablePassThrough();
-        }
+  for (const layer of roomLayers) {
+    if (layer.name === "cameras") {
+      cameras.push(...layer.objects);
     }
 
+    if (layer.name === "positions") {
+      positions.push(...layer.objects);
+      continue;
+    }
+    if (layer.name === "colliders") {
+      colliders.push(...layer.objects);
+      continue;
+    }
+  }
 
+  setMapColliders(k, map, colliders);
+  setCameraZones(k, map, cameras);
+
+  const player = k.add(makePlayer(k));
+
+  setCameraControls(k, player, map, roomData);
+
+  for (const position of positions) {
+    if (position.name === "player") {
+      player.setPosition(position.x, position.y);
+      player.setControls();
+      player.setEvents();
+      player.enablePassThrough();
+    }
+  }
 }

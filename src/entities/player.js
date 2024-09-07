a7257640ec2382494344e7adf0633d8f31c7062d
Sriram N V <sriramnvilvanathan@gmail.com>
2024-09-07 19:14:41 +0530

    removing mouse controls

--- src/entities/player.js
+++ src/entities/player.js
@@ -83,27 +83,7 @@ export function makePlayer(k) {
               this.move(this.speed, 0);
               return;
             }
-          }),
-          // k.onMouseDown((key)=>{
-          //   if (key === "left" && !this.isAttacking) {
-          //     if (this.curAnim() !== "run" && this.isGrounded()) {
-          //       this.play("run");
-          //     }
-          //     this.flipX = true;
-          //     this.move(-this.speed, 0);
-          //     return;
-          //   }
-
-          //   if (key === "right" && !this.isAttacking) {
-          //     if (this.curAnim() !== "run" && this.isGrounded()) {
-          //       this.play("run");
-          //     }
-          //     this.flipX = false;
-          //     this.move(this.speed, 0);
-          //     return;
-          //   }
-          // })
-          
+          })
         );
 
         this.controlHandlers.push(

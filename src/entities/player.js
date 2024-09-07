cb8ce3f8c83bfd0e0322cada7792d2aa6a7e6c43
Sriram N V <sriramnvilvanathan@gmail.com>
2024-09-07 19:12:41 +0530

    trying mouse

--- src/entities/player.js
+++ src/entities/player.js
@@ -65,25 +65,25 @@ export function makePlayer(k) {
         );
 
         this.controlHandlers.push(
-          k.onKeyDown((key) => {
-            if (key === "left" && !this.isAttacking) {
-              if (this.curAnim() !== "run" && this.isGrounded()) {
-                this.play("run");
-              }
-              this.flipX = true;
-              this.move(-this.speed, 0);
-              return;
-            }
+          // k.onKeyDown((key) => {
+          //   if (key === "left" && !this.isAttacking) {
+          //     if (this.curAnim() !== "run" && this.isGrounded()) {
+          //       this.play("run");
+          //     }
+          //     this.flipX = true;
+          //     this.move(-this.speed, 0);
+          //     return;
+          //   }
 
-            if (key === "right" && !this.isAttacking) {
-              if (this.curAnim() !== "run" && this.isGrounded()) {
-                this.play("run");
-              }
-              this.flipX = false;
-              this.move(this.speed, 0);
-              return;
-            }
-          }),
+          //   if (key === "right" && !this.isAttacking) {
+          //     if (this.curAnim() !== "run" && this.isGrounded()) {
+          //       this.play("run");
+          //     }
+          //     this.flipX = false;
+          //     this.move(this.speed, 0);
+          //     return;
+          //   }
+          // }),
           k.onMouseDown((key)=>{
             if (key === "left" && !this.isAttacking) {
               if (this.curAnim() !== "run" && this.isGrounded()) {
